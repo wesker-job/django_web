e76cfd414e3569b8f50e4df7ccfb2ba5d5d1d01c
Morris-Chen <weiren0516@gmail.com>
2014-09-15 16:32:59 +0800

    django v1

---
+++ website/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+#from django.contrib import admin
+from website.views import hello, current_datetime, hours_ahead
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'website.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
+)

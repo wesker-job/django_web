97ad1093ad8ddb49874ee8b7c9439ed33ceb05b3
Morris-Chen <weiren0516@gmail.com>
2014-09-17 16:12:12 +0800

    templates

--- website/views.py
+++ website/views.py
@@ -1,3 +1,5 @@
+from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse
 import datetime
 
@@ -6,8 +8,14 @@ def hello(request):
 
 def current_datetime(request):
   now = datetime.datetime.now()
-  html = "<html><body>It is now %s.</body></html>" % now
+  t = get_template('current_datetime.html')
+  html = t.render(Context({'current_date':now}))
   return HttpResponse(html)
+
+#def current_datetime(request):
+#  now = datetime.datetime.now()
+#  html = "<html><body>It is now %s.</body></html>" % now
+#  return HttpResponse(html)
 
 def hours_ahead(request, offset):
   try:

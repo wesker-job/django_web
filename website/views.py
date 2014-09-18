bcad12b49ebbd64bfa93763d000e20d4c7330a49
Morris-Chen <weiren0516@gmail.com>
2014-09-18 16:37:20 +0800

    django ch4

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
-from django.template.loader import get_template
-from django.template import Context
+#from django.template.loader import get_template
+#from django.template import Context
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
 import datetime
 
@@ -7,22 +8,35 @@ def hello(request):
   return HttpResponse("Hello World!")
 
 def current_datetime(request):
-  now = datetime.datetime.now()
-  t = get_template('current_datetime.html')
-  html = t.render(Context({'current_date':now}))
-  return HttpResponse(html)
+  current_date = datetime.datetime.now()
+  return render_to_response('test/current_datetime.html', locals())
+
+  #now = datetime.datetime.now()
+  #return render_to_response('current_datetime.html',{'current_date':now})
+  
+  #t = get_template('current_datetime.html')
+  #html = t.render(Context({'current_date':now}))
+  #return HttpResponse(html)
 
 #def current_datetime(request):
 #  now = datetime.datetime.now()
 #  html = "<html><body>It is now %s.</body></html>" % now
 #  return HttpResponse(html)
 
-def hours_ahead(request, offset):
+#def hours_ahead(request, hour_offset):
+  #try:
+  #  hour_offset = int(hour_offset)
+  #except ValueError:
+  #  raise Http404()
+  #next_time = datetime.datetime.now() + datetime.timedelta(hours=hour_offset)
+  #return render_to_response('test/hours)ahead.html',{'hour_offset':hour_offset,'next_time':next_time})
+  #return render_to_response('test/hours_ahead.html', locals())
+  #html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+  #return HttpResponse(html)
+def hours_ahead(request, hour_offset):
   try:
-    offset = int(offset)
+    hour_offset = int(hour_offset)
   except ValueError:
     raise Http404()
-  dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-  html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-  return HttpResponse(html)
-
+  next_time = datetime.datetime.now() + datetime.timedelta(hours=hour_offset)
+  return render_to_response('test/hours_ahead.html', ({'hour_offset':hour_offset,'next_time':next_time}))

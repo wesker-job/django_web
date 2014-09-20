83e7de5ef8f6a9a39c6d93ee861a8389af5c00fa
Morris-Chen <weiren0516@gmail.com>
2014-09-20 11:29:35 +0800

    setting.py

--- website/settings.py
+++ website/settings.py
@@ -77,10 +77,10 @@ DATABASES = {
     #}
   }
 =======
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
 >>>>>>> bcad12b49ebbd64bfa93763d000e20d4c7330a49
 }
 

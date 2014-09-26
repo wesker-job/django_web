e4c1eb538fe5d754c330783299f07cb763e99fc9
Morris-Chen <weiren0516@gmail.com>
2014-09-26 16:29:15 +0800

    django form

--- books/views.py
+++ books/views.py
@@ -1,13 +1,45 @@
 from django.shortcuts import render_to_response
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import send_mail
+from books.models import Book
 
 # Create your views here.
+def contact(request):
+  errors = []
+  if request.method == 'POST':
+    if not request.POST.get('subject',False):
+      errors.append('Enter a subject')
+    if not request.POST.get('message',False):
+      errors.append('Enter a message')
+    if request.POST.get('email') and '@' not in request.POST['mail']:
+      errors.append('Enter a valid e-mail address')
+  #if not errors:
+  #  send_mail(request.POST['subject'],request.POST['message'],request.POST.get('email','noreply@example.com'),['weiren0516@gmail.com'],)
+  return HttpResponseRedirect('/contact/thanks/')
+  return render_to_response('books/contact_form.html', {'errors':errors})
+
 def search(request):
+  error = []
   if 'q' in request.GET:
-    message = "You searched for : %r' % request.GET['q']"
-  else:
-    message = "You submitted an empty form."
-  return HttpResponse(message)
+    q = request.GET['q']
+    if not q:
+      error.append('Enter a search term')
+    elif len(q) > 20:
+      error.append('Please enter at most 20 characters')
+    else:
+      books = Book.objects.filter(title__icontains=q)
+      return render_to_response('books/search_results.html',{'books':books, 'query':q})
+  return render_to_response('books/search_form.html',{'error':error})
+  #if 'q' in request.GET and request.GET['q']:
+  #  q = request.GET['q']
+  #  books = Book.objects.filter(title__icontains=q)
+  #  return render_to_response('books/search_results.html', {'books':books, 'query':q})
+    #message = "You searched for : %r" % request.GET['q']
+  #else:
+  #  return render_to_response('books/search_form.html', {'error':True})
+    #return HttpResponse('Please submit a search term')
+    #message = "You submitted an empty form."
+  #return HttpResponse(message)
 
 def search_form(request):
   return render_to_response('books/search_form.html',)
